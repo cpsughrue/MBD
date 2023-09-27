1acbde9f1fcfeb5118427bf203613145c38eb826
cpsughrue <cpsughrue@gmail.com>
2023-09-27 18:15:41 -0400

    add clean script for killing daemon and removing log files

---
+++ clean.sh
@@ -0,0 +1,9 @@
+# kill running daemon process if exists
+PID=$(ps -aux | grep cc1 | grep -v grep | awk '{print $2}')
+if [[ -n $PID ]]; then
+    kill $PID
+fi
+
+# remove clang directory from tmp if exists
+find /tmp/ -type d -name 'clang-*' 2>/dev/null -exec rm -rf {} \;
+
